979012964bd739367e0d2b49fda538b67ef3b245
Zack Bloom <zackbloom@gmail.com>
2015-01-24 04:42:20 -0500

    Add Eager files

---
+++ install/helper.js
@@ -0,0 +1,21 @@
+window.EagerSisyphusHelper = function(options){
+  if (!document.querySelectorAll) return;
+
+  if (options.all){
+    var forms = document.querySelectorAll('form');
+    if (!forms) return;
+  } else {
+    var forms = [];
+
+    for (var i=0; i < options.forms.length; i++){
+      var els = document.querySelectorAll(options.forms[i]);
+      if (els){
+        forms.push.apply(forms, els);
+      }
+    }
+  }
+
+  for (var i=0; i < forms.length; i++){
+    $(forms[i]).sisyphus();
+  }
+}
