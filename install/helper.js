e2d8e539011c62ce1014b70fc604587e0773d344
Adam Schwartz <aschwartz@hubspot.com>
2015-01-26 20:45:42 -0600

    Switch `all` option to a radio button (and rename to `apply`?)

--- install/helper.js
+++ install/helper.js
@@ -1,21 +1,23 @@
-window.EagerSisyphusHelper = function(options){
+window.EagerSisyphusHelper = function(options) {
   if (!document.querySelectorAll) return;
 
-  if (options.all){
+  if (options.apply === 'all') {
     var forms = document.querySelectorAll('form');
     if (!forms) return;
-  } else {
+  }
+
+  if (options.apply === 'specified') {
     var forms = [];
 
-    for (var i=0; i < options.forms.length; i++){
+    for (var i = 0; i < options.forms.length; i++) {
       var els = document.querySelectorAll(options.forms[i]);
-      if (els){
+      if (els) {
         forms.push.apply(forms, els);
       }
     }
   }
 
-  for (var i=0; i < forms.length; i++){
+  for (var i = 0; i < forms.length; i++) {
     jQuery(forms[i]).sisyphus();
   }
 }
